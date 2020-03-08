478d4149a59f53f3faaee1256f5876ca1dccc152
yash.bhardwaj <yash.bhardwaj@wingify.com>
2020-03-08 08:56:08 +0530

    setup sockets

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
+app.use(express.static("public"));
 
+// routes
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use(express.static("public"));
-
-app.listen(8000, () => {
+var server = app.listen(8000, () => {
   console.log("Example app listening on port 8000!");
+});
+
+var io = socket(server);
+
+io.on("connection", function(socket) {
+  console.log(socket.id);
+  console.log("made scoket connection");
+
+  socket.on("initialise", () => {
+    console.log("initi ");
+    socket.emit("initialise", { init: "yash" });
+  });
+
+  socket.on("chat", (data) => {
+    console.log(data);
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on("feedback", (data) => {
+    console.log(data);
+    socket.broadcast.emit("feedback", data);
+  });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,45 @@
-const socket = io.connect(window.location.host);
-// console.log(window.location.host);
+var socket = io.connect("http://" + window.location.host);
+
+// query dom
+var message = document.getElementById("message");
+var handle = document.getElementById("handle");
+var btn = document.getElementById("send");
+var output = document.getElementById("output");
+var feedback = document.getElementById("feedback");
+
+// emiting events
+btn.addEventListener("click", (event) => {
+  console.log("called click");
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value
+  });
+  message.value = "";
+});
+
+message.addEventListener("keypress", function() {
+  console.log("keypress");
+  socket.emit("feedback", {
+    handle: handle.value
+  });
+});
+
+socket.emit("initialise", {});
+
+// listen for events
+socket.on("chat", (data) => {
+  output.innerHTML +=
+    "<p><strong>" + data.handle + ":</strong>" + data.message + "</p>";
+});
+socket.on("feedback", (data) => {
+  feedback.innerHTML = "<p><em>" + data.handle + " is typing ... </em></p>";
+});
+socket.on("initialise", (data) => {
+  // feedback.innerHTML = "<p><em>" + data.handle + " is typing ... </em></p>";
+  console.log(data);
+});
+
+// clear the typing feedback
+setInterval(() => {
+  feedback.innerHTML = "";
+}, 3000);
